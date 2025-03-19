89eb8a3f47393f8e4fb1f80710c7b68d6d282f03
fluidace <fluidace@proton.me>
2025-03-19 12:51:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Adicione esta linha para corrigir módulos que podem estar no formato errado
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
-    chunkSizeWarningLimit: 500, // Opcional: Aumenta o limite de aviso
+    chunkSizeWarningLimit: 500,
     rollupOptions: {
       output: {
         manualChunks(id) {
